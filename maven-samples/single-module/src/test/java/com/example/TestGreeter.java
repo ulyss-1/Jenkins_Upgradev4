ebff402521da7086d5ef9a48891ba9aedfe192b5
root <root@ip-172-31-20-202.eu-north-1.compute.internal>
2021-01-12 18:15:17 +0000

    2nd commit - cahnegd test text

--- maven-samples/single-module/src/test/java/com/example/TestGreeter.java
+++ maven-samples/single-module/src/test/java/com/example/TestGreeter.java
@@ -33,7 +33,7 @@ public class TestGreeter {
 
     @Test
   public void greetShouldIncludeGreetingMessage() {
-    String someone = "World, Thanks you making me so Much Happy";
+    String someone = "World, Thanks you making me so Such Much Happy!";
 
     assertThat(greeter.greet(someone).length(), is(greaterThan(someone.length())));
   }
